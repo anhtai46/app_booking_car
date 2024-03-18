fc0ae49a29bfe65b6e87899ba3e827e1089a8097
anhtai46 <anhtaibp46@gmail.com>
2024-03-18 11:11:38 +0700

    first commit

---
+++ app/src/main/java/com/example/beecar/Model/Client.java
@@ -0,0 +1,91 @@
+package com.example.beecar.Model;
+
+import java.io.Serializable;
+
+public class Client implements Serializable {
+    int id;
+    String user_name;
+    String password;
+    String full_name;
+    String email;
+    String phone;
+    int user_id;
+
+    public Client(int id, String user_name, String password, String full_name, String email, String phone, int user_id) {
+        this.id = id;
+        this.user_name = user_name;
+        this.password = password;
+        this.full_name = full_name;
+        this.email = email;
+        this.phone = phone;
+        this.user_id = user_id;
+    }
+
+    public Client() {
+    }
+
+    public static final String TB_Name = "tb_client";
+    public static final String COL_id = "id";
+    public static final String COL_user_name = "user_name";
+    public static final String COL_password = "password";
+    public static final String COL_full_name = "full_name";
+    public static final String COL_email = "email";
+    public static final String COL_phone = "phone";
+    public static final String COL_user_id = "user_id";
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
---
+++ app/src/main/java/com/example/beecar/Model/User.java
@@ -0,0 +1,98 @@
+package com.example.beecar.Model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    int id;
+    String user_name;
+    String password;
+    String full_name;
+    String email;
+    String phone;
+    int position;
+
+    public User() {
+    }
+
+    public User(String user_name, String password, String full_name, String email, String phone, int position) {
+        this.user_name = user_name;
+        this.password = password;
+        this.full_name = full_name;
+        this.email = email;
+        this.phone = phone;
+        this.position = position;
+    }
+
+//    public User(String user_name, String password, String full_name, int position) {
+//        this.user_name = user_name;
+//        this.password = password;
+//        this.full_name = full_name;
+//        this.position = position;
+//    }
+
+    public static final String TB_Name = "tb_user";
+    public static final String COL_id = "id";
+    public static final String COL_user_name = "user_name";
+    public static final String COL_password = "password";
+    public static final String COL_email = "email";
+    public static final String COL_phone = "phone";
+    public static final String COL_full_name = "full_name";
+    public static final String COL_position = "position";
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
